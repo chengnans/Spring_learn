31184795214a8fd656368ed269e48243cfbc40e7
chengnan <chengnans@outlook.com>
2024-10-18 14:56:43 +0800

    整合Log4j2

--- spring-first/src/main/java/com/cheng/spring6/User.java
+++ spring-first/src/main/java/com/cheng/spring6/User.java
@@ -1,6 +1,11 @@
 package com.cheng.spring6;
 
 public class User {
+
+    //无参数构造
+    public User(){
+        System.out.println("0:无参数构造执行.....");
+    }
     public void add(){
         System.out.println("add .........");
     }
--- spring-first/src/test/java/TestUser.java
+++ spring-first/src/test/java/TestUser.java
@@ -1,9 +1,15 @@
 import com.cheng.spring6.User;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.lang.reflect.InvocationTargetException;
+
 public class TestUser {
+
+    private Logger logger = LoggerFactory.getLogger(TestUser.class);
     @Test
     public void testUserObject(){
     //加载Spring配置文件，对象创建
@@ -16,5 +22,18 @@ public class TestUser {
     //使用对象调用方法进行测试
         System.out.print("2:");
         user.add();
+        //手动写日志
+        logger.info("###执行调用成功了！");
+    }
+
+    //反射创建对象
+    @Test
+    public void testUserObject1() throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        //获取类Class对象
+        Class clazz = Class.forName("com.cheng.spring6.User");
+        //调用方法创建对象
+        //Object o = clazz.newInstance();
+        User user = (User)clazz.getDeclaredConstructor().newInstance();
+        System.out.println(user);
     }
 }
